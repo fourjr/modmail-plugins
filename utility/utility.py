ef4b6240e323ad97c6ffbc4d62fc145adfddbcf6
fourjr <28086837+fourjr@users.noreply.github.com>
2019-01-26 00:47:28 +0800

    docstrings

--- utility/utility.py
+++ utility/utility.py
@@ -1,12 +1,14 @@
 from discord.ext import commands
 
 
-class Utility:    
+class Utility:
+    """Provides basic utility commands"""
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def say(self, ctx, *, message: commands.clean_content):
+        """Repeats after you"""
         await ctx.send(message)
 
     # TODO: DB Examples
