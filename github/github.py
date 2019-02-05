7739723092b1c6bf99d27ef06d82846ee262008e
fourjr <28086837+fourjr@users.noreply.github.com>
2019-02-05 22:51:12 +0800

    github cog

---
+++ github/github.py
@@ -0,0 +1,15 @@
+import re
+
+
+class Github:
+    """Checks for issue mentions in the message and responds to them.
+    Made for the Modmail server."""
+    async def on_message(self, message):
+        match = re.match(r'modmail(#\d)', message.content)
+        if match:
+            issue_num = match.group(1)
+            await message.channel.send(f'https://github.com/kyb3r/modmail/issues/{issue_num}')
+
+
+def setup(bot):
+    bot.add_cog(Github())
