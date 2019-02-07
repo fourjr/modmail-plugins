b8b2054e2da228040773b45f78a422e5bcd0d821
fourjr <28086837+fourjr@users.noreply.github.com>
2019-02-07 20:07:09 +0800

    fix git plugin

--- github/github.py
+++ github/github.py
@@ -5,7 +5,7 @@ class Github:
     """Checks for issue mentions in the message and responds to them.
     Made for the Modmail server."""
     async def on_message(self, message):
-        match = re.match(r'modmail(#\d)', message.content)
+        match = re.match(r'modmail#(\d*)', message.content)
         if match:
             issue_num = match.group(1)
             await message.channel.send(f'https://github.com/kyb3r/modmail/issues/{issue_num}')
