406967947e52007304abeeb39637f360e25ad78e
Jia Rong Yee <28086837+fourjr@users.noreply.github.com>
2019-12-12 20:24:28 +0800

    add a remove/add thing so votes arent riggged

--- emoji-suggester/emoji-suggester.py
+++ emoji-suggester/emoji-suggester.py
@@ -6,6 +6,10 @@ from discord.ext import commands
 class EmojiSuggestor(commands.Cog):
     """Sets up emoji suggestor channel in Modmail discord."""
 
+    def __init__(self, bot):
+        self.bot = bot
+        self.channel_id = 654622737159159829
+
     async def delete(self, message, warning):
         if warning:
             await message.channel.send(warning, delete_after=5)
@@ -16,7 +20,7 @@ class EmojiSuggestor(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == 654622737159159829:
+        if message.channel.id == self.channel_id:
             if message.author.bot:
                 await asyncio.sleep(5)
                 await self.delete(message, warning=None)
@@ -32,6 +36,27 @@ class EmojiSuggestor(commands.Cog):
             else:
                 await self.delete(message, warning=f'{message.author.mention}, only images + captions are allowed. If you wish to add a caption, edit your original message.')
 
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        if payload.channel_id == self.channel_id:
+            message = await self.bot.get_channel(payload.channel_id).get_message(payload.message_id)
+            for r in message.reactions:
+                if r.count > 1:
+                    try:
+                        await r.remove(self.bot.user)
+                    except discord.NotFound:
+                        pass
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        if payload.channel_id == self.channel_id:
+            message = await self.bot.get_channel(payload.channel_id).get_message(payload.message_id)
+            r_emojis = [r.emoji for r in message.reactions]
+
+            for r in (discord.utils.get(message.guild.emojis, name='check'), discord.utils.get(message.guild.emojis, name='xmark')):
+                if r not in r_emojis:
+                    await message.add_reaction(r)
+
 
 def setup(bot):
-    bot.add_cog(EmojiSuggestor())
+    bot.add_cog(EmojiSuggestor(bot))
