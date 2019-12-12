28b7737911429871210ab48937f2f487ca9b1b4d
Jia Rong Yee <28086837+fourjr@users.noreply.github.com>
2019-12-12 18:19:15 +0800

    Update emoji-suggester.py

--- emoji-suggester/emoji-suggester.py
+++ emoji-suggester/emoji-suggester.py
@@ -11,21 +11,33 @@ class EmojiSuggestor(commands.Cog):
         if message.channel.id == 654622737159159829:
             if message.author.bot:
                 await asyncio.sleep(5)
-                await message.delete()
+                try:
+                    await message.delete()
+                except discord.NotFound:
+                    pass
             elif len(message.attachments):
                 if len(message.attachments) > 1:
                     await message.channel.send(f'{message.author.mention}, send 1 emoji at a time.', delete_after=5)
-                    await message.delete()
+                    try:
+                        await message.delete()
+                    except discord.NotFound:
+                        pass
                 elif not message.attachments[0].filename.endswith('.png'):
                     await message.channel.send(f'{message.author.mention}, only png attachments are allowed.', delete_after=5)
-                    await message.delete()
+                    try:
+                        await message.delete()
+                    except discord.NotFound:
+                        pass
                 else:
                     await message.add_reaction(discord.utils.get(message.guild.emojis, name='check'))
                     await asyncio.sleep(0.1)
                     await message.add_reaction(discord.utils.get(message.guild.emojis, name='xmark'))
             else:
                 await message.channel.send(f'{message.author.mention}, only images + captions are allowed. If you wish to add a caption, edit your original message.', delete_after=5)
-                await message.delete()
+                try:
+                    await message.delete()
+                except discord.NotFound:
+                    pass
 
 
 def setup(bot):
